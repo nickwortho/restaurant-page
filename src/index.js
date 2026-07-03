294ab57a1a03678b8991f71f06a49be72b60f3f8
Nick Worthington <nickrworthington@gmail.com>
2026-07-03 22:42:07 +1000

    pages can be navigated through buttons in the header

--- src/index.js
+++ src/index.js
@@ -6,3 +6,38 @@ import { about } from "./about.js";
 import { contact } from "./contact.js";
 
 home();
+
+const tabBtns = document.querySelectorAll("nav button");
+for (const btn of tabBtns) {
+    console.log(btn.id);
+    btn.addEventListener("click", () => pageHandler(btn.id));
+}
+
+function clearContent() {
+    const content = document.querySelector("#content");
+    content.replaceChildren();
+}
+
+function pageHandler(tab) {
+    clearContent();
+    console.log(tab);
+    switch (tab) {
+        case "home":
+            home();
+            break;
+        case "menu":
+            menu();
+            break;
+        case "events":
+            events();
+            break;
+        case "about":
+            about();
+            break;
+        case "contact":
+            contact();
+            break;
+        default:
+            home();
+    }
+}
