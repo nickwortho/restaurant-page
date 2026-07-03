150129b5975e19b0ef56e7464eb4f0d6d8133b62
Nick Worthington <nickrworthington@gmail.com>
2026-07-03 22:15:58 +1000

    restructured homepage content layout and styling

--- src/initial.js
+++ src/initial.js
@@ -4,18 +4,23 @@ export const initial = (() => {
 
     const content = document.querySelector("#content");
 
+    const heroContainer = document.createElement("div");
+    heroContainer.id = "hero";
+
     const logo = document.createElement("img");
-    logo.setAttribute("src", logoImg);
-    logo.setAttribute("alt", "logo");
-    logo.setAttribute("width", "100px");
+    logo.src = logoImg;
+    logo.alt = "logo";
+    logo.width = 100;
+    logo.height = 100;
+    heroContainer.appendChild(logo);
 
     const hero = document.createElement("h1");
     hero.textContent = "Welcome to <Restaurant Name>!";
+    heroContainer.appendChild(hero);
 
     const splashText = document.createElement("p");
     splashText.textContent = "Come and try our amazing food!";
+    heroContainer.appendChild(splashText);
 
-    content.appendChild(logo);
-    content.appendChild(hero);
-    content.appendChild(splashText);
+    content.appendChild(heroContainer);
 });
